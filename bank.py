fb55f72799018a4cf186a6ed6d32512df650ea3d
admin <yahromskaya@gmail.com>
2023-12-28 18:32:04 +0300

    первый релиз программы

---
+++ bank.py
@@ -0,0 +1,41 @@
+import sqlite3
+import random
+class Bank:
+    def __init__(self, path):
+        self.con = sqlite3.connect(path)
+        self.cursor = self.con.cursor()
+
+    def create_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS USERS(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        LOGIN VARCHAR(255) NOT NULL,
+        PASSWORD VARCHAR(255) NOT NULL,
+        CASH INTEGER NOT NULL
+         )''')
+        #self.con.commit()
+    def auth_login(self, login):
+        self.cursor.execute('select * from users where login=?', (login,))
+        if self.cursor.fetchall():
+            return True
+        return False
+
+    def generate_users(self):
+        for user in range(1, 101):
+            self.cursor.execute('insert into users (login, password, cash) values(?,?,?)', ('user'+str(user), self.generate_password(), self.generate_cash()))
+            self.con.commit()
+
+    def generate_password(self):
+        pass_s = 'fsdfglkwlaskflaskf2424@3$adfgasfadfgg45645747ukyu'
+        password = ''
+        for i in range(5):
+            password += pass_s[random.randint(0, len(pass_s) - 1)]
+        return password
+
+    def generate_cash(self):
+        return random.randint(1000, 10000)
+
+    def auth_password(self, login, password):
+        self.cursor.execute('select * from users where login = ? and password=?', (login, password,))
+        if self.cursor.fetchall():
+            return True
+        return False
---
+++ main.py
@@ -0,0 +1,19 @@
+from bank import Bank
+import time
+bank = Bank('bank.db')
+bank.create_table()
+
+while True:
+    #запросить у пользователя логин и пароль если есть в базе то запросить баланс
+    login=input("Login: ")
+    if bank.auth_login(login):
+        password=input("Password: ")
+        if bank.auth_password(login, password):
+            print("Вход успешно выполнен")
+            time.sleep(2)
+            print("1 - снять\n2 - внести\n3 - изменить пароль")
+
+        else:
+            print('Пароль введен неправильно')
+    else:
+        print("Логин введен неправильно")
